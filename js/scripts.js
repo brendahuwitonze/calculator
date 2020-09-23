7d8875232408b10c08974bb2ed00316dfe84bba7
uwitonze brendah <uwitonzebrendah@gmail.com>
2020-09-23 20:28:40 +0200

    added a function to divide twa numbers

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,14 @@ var add = function(number1, number2) {
       var number2=parseInt(prompt("enter a number"));
       var result=multiply(number1,number2);
       alert(result);
+
+  
+
+    var divide=function(number1,number2){
+      return number1/number2
+      };
+      
+      var number1= parseInt(prompt("enter  a number"));
+      var number2=parseInt(prompt("enter a number"));
+      var result=divide(number1,number2);
+      alert(result);
